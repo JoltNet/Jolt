511c06c9b09b615684cfecdb7213bff402360984
Jozef Izso <jozef.izso@gmail.com>
2014-11-18 00:24:38 +0100

    Added strong name to Jolt.Testing and bumped version to v0.5

--- Jolt/Jolt.Testing/Properties/AssemblyInfo.cs
+++ Jolt/Jolt.Testing/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Jolt Testing Library")]
-[assembly: AssemblyDescription("Contains classes supporting the development of strong unit tests and injectable components.")]
+[assembly: AssemblyDescription("Contains classes for testing XML equality.")]
 //[assembly: AssemblyConfiguration("")]
 //[assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Jolt.Testing")]
@@ -28,13 +28,8 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("0.4.*")]
-[assembly: AssemblyFileVersion("0.4.*")]
+[assembly: AssemblyVersion("0.5.0.0")]
+[assembly: AssemblyFileVersion("0.5.0.0")]
 
 // Friend assemblies.
-[assembly: InternalsVisibleTo("Jolt.Testing.Test")]
-[assembly: InternalsVisibleTo("Jolt.Testing.Assertions.NUnit")]
-[assembly: InternalsVisibleTo("Jolt.Testing.Assertions.NUnit.Test")]
-[assembly: InternalsVisibleTo("Jolt.Testing.Assertions.VisualStudio")]
-[assembly: InternalsVisibleTo("Jolt.Testing.Assertions.VisualStudio.Test")]
-[assembly: InternalsVisibleTo("DynamicProxyGenAssembly2")]
+[assembly: InternalsVisibleTo("Jolt.Testing.Test, PublicKey=0024000004800000140100000602000000240000525341310008000001000100b3ec59cc534aaadee1abfd5d0b639bd41c693322002a34f9a50346b75d10ebe19c27fd82f0bfdc4d898742f7fa6c58d0bad6c7bf3560c138b765380bfe0d144f41dd3a2fbf7b7534a9a14ea8e7532a3345c7c8e36c3d316ad24828bcd055fa333fe93acf4749ce5cb77cb3855427941785b99bd02d7fa3bc0bb3561bfa2e730446909127f868a5b45309b3bc054a90d5d1013979cacb56bf5cea487f9544831ad009ff47fa8574e471336e703d89c06aad522c2f42d1054675f54f56d135f897509adc8576b54d334294eec3d288fcb12e9c58937ee3142e51cc7f40e54db50ca3886b00a5f1fd7b4acc06230f83fa7e8aa9ca9753fa4d3ef849165dfddd0fce")]
